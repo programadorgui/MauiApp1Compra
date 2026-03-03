792e21622d358e074d13bb5e6af34e4aa2789cbc
programdorgui <siegmatos@gmail.com>
2026-03-03 01:55:29 -0300

    add

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiApp1.Helpers;
 
 namespace MauiApp1
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+
+        public static SQLiteDatabaseHelper Db {
+
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper("..... db3");
+                }
+
+
+
+                return _db;
+            }
+         
+        }
         public App()
         {
             InitializeComponent();
--- MauiApp1/Views/NovoProduto.xaml.cs
+++ MauiApp1/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiApp1.Models;
+
 namespace MauiApp1.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,27 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+
+			Produto p = new Produto
+			{
+
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+        
+
+
+		} catch(Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
 }
